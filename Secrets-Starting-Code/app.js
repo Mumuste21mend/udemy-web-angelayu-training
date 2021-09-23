5a0a5c1439db5962c03640c0af9c2c87a2069794
Must <Mumuste.mend@gmail.com>
2021-09-23 22:11:04 +0100

    LEVEL 3 security HASHING with MD5 algorithm

--- Secrets-Starting-Code/app.js
+++ Secrets-Starting-Code/app.js
@@ -2,28 +2,25 @@ require('dotenv').config();
 const express = require( "express");
 const ejs = require( "ejs");
 const mongoose = require( "mongoose");
-const encrypt = require( "mongoose-encryption");
+const md5 = require("md5");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 
-const SECRET_KEY=process.env.SECRET_KEY;
 const DB_HOST=process.env.DB_HOST;
 const DB_PORT=process.env.DB_PORT;
 const DB_NAME=process.env.DB_NAME;
 const PORT=process.env.PORT;
 
-mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`,{useNewUrlParser:true});
+ mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`,{useNewUrlParser:true});
 
 const userSchema=new mongoose.Schema({
     email:String,
     password:String
 });
 
-
-userSchema.plugin(encrypt,{secret:SECRET_KEY,encryptedFields:['password']});
 
 const User=mongoose.model("User",userSchema);
 
@@ -35,13 +32,13 @@ app.route("/login")
     res.render("login");
 })
 .post((req,res)=>{
-     const [email,password]=  [req.body.username,req.body.password];
+     const [email,password]=  [req.body.username,md5(req.body.password)];
     User.findOne({email:email})
     .then( user=>{
         (user.password===password)?res.render("secrets",{user:{email,password}}):console.log("password unmatched")})
     .catch(console.error);
 });
-//post.meh@tit.com
+
 app.route("/register")
 .get((req,res)=>{
     res.render("register");
@@ -49,11 +46,11 @@ app.route("/register")
 .post((req,res)=>{
     const aUser= new User( {    
         email:req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
         });
         aUser.save().then(res.render("secrets",{user:aUser})).catch(console.error);
     
 })
 app.listen(PORT,()=>{
-    console.log(`successfully connecting to port${PORT}`);
+    console.log(`successfully connecting to port ${PORT}`);
 })
