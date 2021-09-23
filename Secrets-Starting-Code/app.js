531a3c5999911786b6974247746a9820f0ee1b98
Must <Mumuste.mend@gmail.com>
2021-09-23 23:03:30 +0100

    LEVEL4 security SALTING and Bcrypt with salt rounds

--- Secrets-Starting-Code/app.js
+++ Secrets-Starting-Code/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require( "express");
 const ejs = require( "ejs");
 const mongoose = require( "mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
 const app = express();
 
 app.set("view engine", "ejs");
@@ -13,6 +13,9 @@ const DB_HOST=process.env.DB_HOST;
 const DB_PORT=process.env.DB_PORT;
 const DB_NAME=process.env.DB_NAME;
 const PORT=process.env.PORT;
+
+const saltRounds = 10;
+
 
  mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`,{useNewUrlParser:true});
 
@@ -32,10 +35,16 @@ app.route("/login")
     res.render("login");
 })
 .post((req,res)=>{
-     const [email,password]=  [req.body.username,md5(req.body.password)];
-    User.findOne({email:email})
-    .then( user=>{
-        (user.password===password)?res.render("secrets",{user:{email,password}}):console.log("password unmatched")})
+     const [email,password]=  [req.body.username,req.body.password];
+     User.findOne({email:email})
+    .then(user=>user? 
+                bcrypt
+                    .compare(password, user.password)
+                    .then((result)=>
+                        result?
+                        res.render("secrets",{user:{email,password}}):
+                        console.log("password unmatched"))
+                :console.log("email does not exist"))
     .catch(console.error);
 });
 
@@ -44,12 +53,17 @@ app.route("/register")
     res.render("register");
 })
 .post((req,res)=>{
-    const aUser= new User( {    
-        email:req.body.username,
-        password: md5(req.body.password)
-        });
-        aUser.save().then(res.render("secrets",{user:aUser})).catch(console.error);
-    
+    bcrypt
+    .hash(req.body.password, saltRounds)
+    .then(hash=> {
+        // Store hash in your password DB.
+        const aUser= new User( {    
+            email:req.body.username,
+            password:hash
+            });
+            aUser.save().then(res.render("secrets",{user:aUser}))})
+    .catch(console.error);;
+   
 })
 app.listen(PORT,()=>{
     console.log(`successfully connecting to port ${PORT}`);
