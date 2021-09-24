5964696eb627931051420dbb302d351d97041599
Must <Mumuste.mend@gmail.com>
2021-09-24 18:52:04 +0100

    LEVEL 5 security Using passport.js to Add Cookies and sessions

--- Secrets-Starting-Code/app.js
+++ Secrets-Starting-Code/app.js
@@ -1,69 +1,96 @@
 require('dotenv').config();
-const express = require( "express");
-const ejs = require( "ejs");
-const mongoose = require( "mongoose");
-const bcrypt = require('bcrypt');
-const app = express();
-
-app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(__dirname + "/public"));
 
 const DB_HOST=process.env.DB_HOST;
 const DB_PORT=process.env.DB_PORT;
 const DB_NAME=process.env.DB_NAME;
-const PORT=process.env.PORT;
+const PORT=process.env.PORT; 
 
-const saltRounds = 10;
+const express = require( "express");
+const ejs = require( "ejs");
+const mongoose = require( "mongoose");
+const app = express();
+const session = require("express-session");
+const passport = require("passport");
+// const passportLocalMongoose = require("passport-local-mongoose");
 
+const User=require("./models/user");
 
- mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`,{useNewUrlParser:true});
+app.set("view engine", "ejs");
 
-const userSchema=new mongoose.Schema({
-    email:String,
-    password:String
-});
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"));
 
+app.use(session({
+    secret: 'dead always fed',
+    resave: false,
+    saveUninitialized: true,
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const User=mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`,{useNewUrlParser:true});
+
+ // schema and model are all in their module file
 
 app.route("/").get((req,res)=>{
     res.render("home");
 })
 app.route("/login")
+
 .get((req,res)=>{
-    res.render("login");
+  req.isAuthenticated()?res.redirect("/secrets"):res.render("login");
 })
-.post((req,res)=>{
-     const [email,password]=  [req.body.username,req.body.password];
-     User.findOne({email:email})
-    .then(user=>user? 
-                bcrypt
-                    .compare(password, user.password)
-                    .then((result)=>
-                        result?
-                        res.render("secrets",{user:{email,password}}):
-                        console.log("password unmatched"))
-                :console.log("email does not exist"))
-    .catch(console.error);
+/* using passport.authenticate as a middleware that 
+sets the req.user  invoke req.login() */
+.post( 
+    // passport.authenticate("local",{
+    //     successRedirect: "/secrets",
+    //     failureRedirect: "/login"
+    // }),
+
+    
+    (req,res)=>{
+    const user = new User({
+        username:req.body.username.trim(),
+        password:req.body.password
+        });
+    req.login(user,(err)=>{//create session 
+       if(err){return next(err);}
+       passport.authenticate("local")(req,res,()=>{ // authenticate and create session 
+        res.redirect("/secrets");
+    })
+        });
+    }
+);
+
+app.get("/register",(req,res)=>{
+    res.render("register");
 });
 
-app.route("/register")
-.get((req,res)=>{
-    res.render("register");
+app.post("/register",(req,res)=>{
+    User.register({username:req.body.username},req.body.password)
+    .then(user=> passport.authenticate("local")(req,res,()=>{
+        res.redirect("/secrets");
+    }))
+    .catch(err=>{console.error(err);res.redirect("/register")})
 })
-.post((req,res)=>{
-    bcrypt
-    .hash(req.body.password, saltRounds)
-    .then(hash=> {
-        // Store hash in your password DB.
-        const aUser= new User( {    
-            email:req.body.username,
-            password:hash
-            });
-            aUser.save().then(res.render("secrets",{user:aUser}))})
-    .catch(console.error);;
-   
+  const isLoggedIn=(req,res,next)=>{
+    if(req.isAuthenticated()){
+        return next();
+    }
+        res.redirect("/login")
+}
+app.get("/secrets",isLoggedIn,(req,res)=>{
+   res.render("secrets");
+})
+ 
+app.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect('/login');
 })
 app.listen(PORT,()=>{
     console.log(`successfully connecting to port ${PORT}`);
